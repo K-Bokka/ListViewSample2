766184d3840dab8dd9ed1a708c7d54722214c077
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2020-01-05 14:09:46 +0900

    Set dialog to activity listener

--- app/src/main/java/com/kbokka/android/listviewsample2/MainActivity.kt
+++ app/src/main/java/com/kbokka/android/listviewsample2/MainActivity.kt
@@ -2,6 +2,8 @@ package com.kbokka.android.listviewsample2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.ListView
 
@@ -19,5 +21,14 @@ class MainActivity : AppCompatActivity() {
     val adapter = ArrayAdapter(applicationContext, android.R.layout.simple_list_item_1, menuList)
 
     lvMenu.adapter = adapter
+    lvMenu.onItemClickListener = ListItemClickListener()
+  }
+
+  private inner class ListItemClickListener : AdapterView.OnItemClickListener {
+    override fun onItemClick(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+
+      val dialogFragment = OrderConfirmDialogFragment()
+      dialogFragment.show(supportFragmentManager, "OrderConfirmDialogFragment")
+    }
   }
 }
